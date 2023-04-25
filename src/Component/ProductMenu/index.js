4e3d5ec9c7450c49a3a4530d41b215ca98e6a990
tungle203 <thanhtunga52021@gmail.com>
2023-04-25 13:33:30 +0700

    modifyCard

--- src/Component/ProductMenu/index.js
+++ src/Component/ProductMenu/index.js
@@ -1,24 +1,32 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
-import styles from './ProductMenu.module.css';
 
-import { Product } from '../../Class/Product';
+import { productList } from '../../Class/Product';
 import ProductCard from '../ProductCard';
+import ModifyCard from '../ModifyCard';
+import styles from './ProductMenu.module.css';
 
 const cx = classNames.bind(styles);
 
 function ProductMenu() {
-  const product1 = new Product('Sản phẩm 1', 100000);
-  const product2 = new Product('Sản phẩm 2', 100000);
-  const product3 = new Product('Sản phẩm 3', 100000);
-  const product4 = new Product('Sản phẩm 4', 100000);
-  const productList = [product1, product2, product3, product4];
+  const [show, setShow] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(productList[0]);
   return (
     <div className={cx('wrapper')}>
       <h4>Đồ ăn</h4>
       <div className={cx('list')}>
         {productList.map((product, index) => (
-          <li key={index}>{<ProductCard product={product} />}</li>
+          <li
+            key={index}
+            onClick={() => {
+              setShow(true);
+              setCurrentProduct(product);
+            }}
+          >
+            {<ProductCard product={product} />}
+          </li>
         ))}
+        {<ModifyCard classNames={show ? 'show' : 'hide'} product={currentProduct} />}
       </div>
     </div>
   );
