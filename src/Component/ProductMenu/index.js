2a5d5d1aa3436ab02936b573adf50f3b8d911060
Cương Phan <phanthecuong2210@gmail.com>
2023-04-25 13:25:28 +0700

    css add page, sidebar

--- src/Component/ProductMenu/index.js
+++ src/Component/ProductMenu/index.js
@@ -7,14 +7,22 @@ import ProductCard from '../ProductCard';
 const cx = classNames.bind(styles);
 
 function ProductMenu() {
-  const product1 = new Product('Sản phẩm 1', 100000);
-  const product2 = new Product('Sản phẩm 2', 100000);
-  const product3 = new Product('Sản phẩm 3', 100000);
-  const product4 = new Product('Sản phẩm 4', 100000);
-  const productList = [product1, product2, product3, product4];
+  const product1 = new Product('Mỳ tôm', 100000);
+  const product2 = new Product('Sandwich', 100000);
+  const product3 = new Product('Kẹo', 100000);
+  const product4 = new Product('Bánh ngọt', 100000);
+  const product5 = new Product('Oishi', 100000);
+  const productList = [product1, product2, product3, product4, product5];
   return (
     <div className={cx('wrapper')}>
       <h4>Đồ ăn</h4>
+      <div className={cx('list')}>
+        {productList.map((product, index) => (
+          <li key={index}>{<ProductCard product={product} />}</li>
+        ))}
+      </div>
+
+      <h4>Thức uống</h4>
       <div className={cx('list')}>
         {productList.map((product, index) => (
           <li key={index}>{<ProductCard product={product} />}</li>
