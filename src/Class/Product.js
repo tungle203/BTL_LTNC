6c8303b6817495d9bd02d422e0281a3e73296ef4
Cương Phan <phanthecuong2210@gmail.com>
2023-04-28 13:04:24 +0700

    css Siêu cấp pro

--- src/Class/Product.js
+++ src/Class/Product.js
@@ -156,14 +156,14 @@ class Food extends Product {
 }
 
 const Coca = new Beverage('Coca Cola', 10000, 330, 'Nguyên Bản');
-const Pepsi = new Beverage('Pepsi', 10000, 330, 'Nguyên Bản');
+const Suprite = new Beverage('Suprite', 10000, 330, 'Nguyên Bản');
 const Vinamilk = new Beverage('Vinamilk', 8000, 250, 'Sữa tươi tiệt trùng');
 const Huda = new Beverage('Bia Huda', 18000, 330, 'Đậm tình miền Trung');
 
-const BanhPia = new Food('Bánh Pía', 20000, 100, 500);
+const Sandwich = new Food('Sandwich', 20000, 100, 500);
 const BanhBongLan = new Food('Bánh Bông Lan', 10000, 50, 100);
-const CaTuoi = new Food('Cá Tươi', 100000, 1000, 1000);
-const ChanGa = new Food('Chân gà', 20000, 100, 400);
+const MyTom = new Food('Mỳ tôm', 100000, 1000, 1000);
+const Oishi = new Food('Oishi', 20000, 100, 400);
 
-export const BeverageList = [Coca, Pepsi, Vinamilk, Huda];
-export const FoodList = [BanhPia, BanhBongLan, CaTuoi, ChanGa];
+export const BeverageList = [Coca, Suprite, Vinamilk, Huda];
+export const FoodList = [Sandwich, BanhBongLan, MyTom, Oishi];
